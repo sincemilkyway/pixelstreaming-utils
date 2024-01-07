91fb18548c708703532962e627fdcc1c95274bb1
Sourav  Padhi <97221197+heysouravv@users.noreply.github.com>
2024-01-07 12:20:41 +0530

    Sun Jan  7 12:20:41 IST 2024

--- create-record.py
+++ create-record.py
@@ -32,7 +32,7 @@ def create_application_subdomain(ip_address):
                 'Changes': [{
                     'Action': 'CREATE',
                     'ResourceRecordSet': {
-                        'Name': domain,
+                        'Name': subdomain,
                         'Type': 'A',
                         'TTL': 300,
                         'ResourceRecords': [{'Value': ip_address}]
